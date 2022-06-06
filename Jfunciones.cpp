7546b7bac8ba63f1665a3bcc32a39e717fc98495
Maijuanem <70459432+Maijuanem@users.noreply.github.com>
2022-06-06 13:58:00 -0300

    Update Jfunciones.cpp

--- Jfunciones.cpp
+++ Jfunciones.cpp
@@ -1,4 +1,5 @@
 #include "Jfunciones.hpp"
+
 void limpiarBuffer(Stream *puerto){
   while(puerto->available()>0) puerto->read();
 }
@@ -8,7 +9,13 @@ void limpiarCadena(char* limpiar, int largo){
     limpiar[i]='\0';
   }
 }
-
+void setTimer1PRE(uint8_t nibble){
+  TCCR1B |= (((nibble&2)>>1)<<CS12) | (((nibble&4)>>2)<<CS11) | (nibble&1<<CS10);
+}
+void setTimer1WGM(uint8_t nibble){
+   TCCR1B |= (((nibble&8)>>3)<<WGM13) | (((nibble&4)>>2)<<WGM12);
+   TCCR1A |= (((nibble&2)>>1)<<WGM11) | ((nibble&1)<<WGM10);
+}
 void setOC1A(uint8_t nibble){
   if (nibble > 3) nibble = 0;
   TCCR1A |= (((nibble&2)>>1)<<COM1A1) | ((nibble&1)<<COM1A0) ;
@@ -16,13 +23,4 @@ void setOC1A(uint8_t nibble){
 void setOC1B(uint8_t nibble){
   if (nibble > 3) nibble = 0;
   TCCR1A |= (((nibble&2)>>1)<<COM1B1) | ((nibble&1)<<COM1B0) ;
-}
-void setTimer1PRE(uint8_t nibble){
-  if (nibble > 7) nibble = 0;
-  TCCR1B |= (((nibble&2)>>1)<<CS12) | (((nibble&4)>>2)<<CS11) | (nibble&1<<CS10);
-}
-void setTimer1WGM(uint8_t nibble){
-  if (nibble > 15) nibble = 0;
-  TCCR1B |= (((nibble&8)>>3)<<WGM13) | (((nibble&4)>>2)<<WGM12);
-  TCCR1A |= (((nibble&2)>>1)<<WGM11) | ((nibble&1)<<WGM10);
 }
